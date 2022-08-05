fix bug of gradient

nets/region_proposal_network.py
@@ -38,12 +38,11 @@ class RegionProposalNetwork(nn.Module):
 
     def forward(self, x, image_size, anchor, scale=1.):
 
-        x = f.relu(self.conv(x))
-        pred_cls_scores = torch.sigmoid(self.confidence_classify_layer(x))  # [B,50*50*9,2]
-        pred_locations = self.location_regression_layer(x)  # [B,50,50*9,4]
-        # Todo Sigmoid or Softmax ?
-
         batch_size, _, height, width = x.shape
+        x_relu = f.relu(self.conv(x))
+        pred_cls_scores = torch.sigmoid(self.confidence_classify_layer(x_relu))  # [B,50*50*9,2]
+        pred_locations = self.location_regression_layer(x_relu)  # [B,50,50*9,4]
+        # Todo Sigmoid or Softmax ?
 
         pred_locations = pred_locations.permute(0, 2, 3, 1) \
             .contiguous() \

targets/proposal_creator.py
@@ -2,6 +2,7 @@ import torch
 import torchvision.ops
 
 from utils.bbox_tools_torch import cvt_location_to_bbox
+from utils.to_tensor import to_device
 
 
 class ProposalCreator(object):
@@ -58,10 +59,10 @@ class ProposalCreator(object):
 
         # 将回归值恢复到原图检测框
         roi = cvt_location_to_bbox(pred_locations, anchor)
-
-        # 尺寸裁剪至图像尺寸内
-        roi[:, slice(0, 4, 2)] = torch.clip(roi[:, slice(0, 4, 2)], 0, img_size[0])
-        roi[:, slice(1, 4, 2)] = torch.clip(roi[:, slice(1, 4, 2)], 0, img_size[1])
+        roi_new = to_device(torch.empty_like(roi))
+        # Fixed bug 梯度原地踏步 尺寸裁剪至图像尺寸内
+        roi_new[:, slice(0, 4, 2)] = torch.clip(roi[:, slice(0, 4, 2)], 0, img_size[0])
+        roi_new[:, slice(1, 4, 2)] = torch.clip(roi[:, slice(1, 4, 2)], 0, img_size[1])
 
         # 先按 min_size_threshold 抑制不符合的候选框
         min_size = self.min_size_threshold * scale

train.py
@@ -13,8 +13,10 @@ from targets.anchor_target_creator import AnchorTargetCreator
 from utils.anchor import generate_anchor_base, enumerate_shifted_anchor
 from utils.to_tensor import cvt_module
 
+torch.autograd.set_detect_anomaly(True)
+
 net = cvt_module(FasterRCNN())
-loss_net = cvt_module(ROILoss())
+loss_net = cvt_module(FinalLoss())
 anchor_target_creator = AnchorTargetCreator()
 
 anchor = enumerate_shifted_anchor(generate_anchor_base(), 16, 50, 50)
@@ -61,18 +63,18 @@ for epoch in range(trainPlan.epoch):  # loop over the dataset multiple times
         #                 rpn_locs.view(-1, 4),
         #                 gt_rpn_label.view(-1),
         #                 gt_rpn_loc.view(-1, 4), )
-        loss = loss_net(roi_scores,
+        # loss = loss_net(roi_scores,
+        #                 roi_cls_locs,
+        #                 gt_roi_labels.view(-1),
+        #                 gt_roi_locs)
+        loss = loss_net(rpn_scores.view(-1, 2),
+                        rpn_locs.view(-1, 4),
+                        roi_scores,
                         roi_cls_locs,
+                        gt_rpn_label.view(-1),
+                        gt_rpn_loc.view(-1, 4),
                         gt_roi_labels.view(-1),
                         gt_roi_locs)
-        # loss = final_loss(rpn_scores.view(-1, 2),
-        #                   rpn_locs.view(-1, 4),
-        #                   roi_scores,
-        #                   roi_cls_locs,
-        #                   gt_rpn_label.view(-1),
-        #                   gt_rpn_loc.view(-1, 4),
-        #                   gt_roi_labels.view(-1),
-        #                   gt_roi_locs)
         # backward
         loss.backward()
 